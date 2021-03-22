033251841c9f63ef1883d873613aeff146d3d110
BEN <benjamin.hill28@gmail.com>
2021-03-21 21:51:09 -0700

    attempted some things, and stuff

--- api/views.py
+++ api/views.py
@@ -1,14 +1,17 @@
-from django.db.models import query
 from rest_framework import generics
 from .serializers import TaskSerializer
 from .models import Task
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = IsAuthenticatedOrReadOnly
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = IsOwnerOrReadOnly
